bddc9f3b814b3992ffe8dc8c8e0d83f11109ef76
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-05 05:48:32 +0500

    Initialize Flutter binding and clean up main.dart: remove unused SQL connection code and commented-out recipe loading logic.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/services.dart';
 import 'package:food_dairy_app/model.dart/RecppeModel.dart';
 import 'package:food_dairy_app/sql/sql.dart';
-import 'package:food_dairy_app/sql/sqllite.dart';
 import 'package:food_dairy_app/screen/loginScreen.dart';
 import 'package:food_dairy_app/widget/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: mainTheme.scaffoldBackgroundColor,
       statusBarBrightness: Brightness.light));
   runApp(const MyApp());
-  await SQL.connection();
-// var list= await loadRecipes();
-//   for (var e in list) {
-    
-//     SQL.get("INSERT INTO dbo.Brecipes VALUES (${e.toMap()})");}
 }
 
 class MyApp extends StatelessWidget {
@@ -30,9 +25,7 @@ class MyApp extends StatelessWidget {
             ColorScheme.fromSeed(seedColor: mainTheme.scaffoldBackgroundColor),
         useMaterial3: true,
       ),
-
       home: const LoginScreen(),
-      // HomeScreen(),
       darkTheme: mainTheme,
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
